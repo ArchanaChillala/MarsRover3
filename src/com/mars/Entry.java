e0cae9bf2b521f4c42bca1603a0663b61599b6e0
ArchanaChillala <archanac@thoughtworks.com>
2015-07-31 12:06:26 +0530

    #0010 | Laundry List added

--- src/com/mars/Entry.java
+++ src/com/mars/Entry.java
@@ -6,10 +6,10 @@ package com.mars;
 public class Entry {
     public static void main(String[] args) {
         View view = new View();
+        view.fixPlateau();
+
         Rover rover1 = new Rover();
         Rover rover2 = new Rover();
-
-        view.fixPlateau();
 
         view.printMessage("\nRover 1 :");
         rover1.getLocation();
